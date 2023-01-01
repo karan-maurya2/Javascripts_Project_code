16ad917c47a4507a0301c0b9587c52d006a93461
Karan Maurya <karanmaurya.connect@gmail.com>
2023-01-01 22:11:08 +0530

    Javascripts Animantion

---
+++ index.js
@@ -0,0 +1,182 @@
+
+    // console.log(typeof 1);
+
+    // let temperature = 60;
+    // console.log(`the temperature is  ${temperature}`);
+
+    // if (temperature > 70) {
+    //     console.log("Outside weather is warm");
+        
+    // }
+
+
+    // if (1 > 2) {
+    //     num = 1;
+    //  }
+    //  console.log(num === 1);
+// ............................................................................................
+// let num=`Rahul is Boy`;
+
+// function func() {
+//     return 67 +6 ;
+//  };
+//  const result = func();
+//  console.log(result);
+
+// ......................................................................
+
+// let num=function () {
+//     return `this is function experession`;
+// };
+// let result = num();
+// console.log(result);
+
+// .............................................................................
+
+// const func = function() {
+//     let text = 'hello';
+//     text = text + ' world';
+//     return text;
+//  };
+//  const result = func();
+//  console.log(result);
+
+// .......................................................................................
+
+//  When JavaScript reaches a return statement, the function will stop executing. hence output is "hello"
+// const func = function () {
+//     let text = 'hello';
+//     return text;
+//     text = text + ' world';
+//     return text;
+//  };
+//  const result = func();
+//  console.log(result);
+
+// .............................................................................................
+
+
+// const multiply = function (num) {
+//     return num * num;
+//  };
+  
+//  const arg = 5;
+  
+//  const result = multiply(arg);
+//  console.log(result === 25);
+// ................................................................
+
+// const buttonElem = document.getElementById("button");
+  
+// buttonElem.addEventListener('click', () => {
+//   const oldText = buttonElem.innerText;
+//   return button.innerText = oldText === "ON" ? "OFF" : "ON";
+// });
+  
+// ........................................................
+// function changeText(id) {
+//     id.innerHTML="opps..";
+// }
+
+//  function displayDate() {
+//     document.getElementById("demo").innerHTML=Date();
+    
+//  }
+
+//  document.getElementById("bdemo").onclick=showdate;
+
+//  function showdate() {
+//     document.getElementById("pdemo").innerHTML=Date();
+//  }
+ 
+
+
+
+
+//  function checkCookies() {
+
+//     if (navigator.cookieEnabled == true) {
+//    console.log( "Cookies are enabled.");
+//     } else {
+//       console.log( "Cookies are not enabled.");
+//     }
+    
+//   }
+
+
+// ...........................................
+
+// function uppercase() {
+
+//   const x=document.getElementById('fname');
+//   x.value=x.value.toLowerCase();
+// }
+
+// function Mover(id) {
+//     id.style.backgroundColor="blue";
+//     id.innerHTML="Thank you"
+    
+// }
+
+// function Mout(id) {
+//     id.style.backgroundColor="yellow";
+//     id.innerHTML="Good Bye"
+    
+// }
+
+
+// const buttonElem = document.getElementById('wrapper');
+// const inputElem =  document.getElementById('fname');
+
+// buttonElem.addEventListener('click', () => {
+//     const oldText = inputElem.value;
+//       return inputElem.value = oldText === "ON" ? "OFF" : "ON";
+//   });
+
+// blur1()
+// {
+
+//     document.getElementById('demo').innerHTML="content changed";
+// }
+
+
+
+// document.getElementById("demo1").innerHTML=document.getElementById("demo2").innerHTML;
+
+
+
+// var x, y, z;
+
+// x=5;
+// y=6;
+// z= x + y;
+
+// document.getElementById('semo').innerHTML = 
+// ("the sum of  x + y =" + z + ".");
+
+// var x, y, z;
+
+// x=5+6;
+// y= x* 10;
+
+
+//  document.getElementById("semo").innerHTML= y;
+
+// let carName = "Volvo";
+// let carName;
+// document.getElementById("demo").innerHTML = carName;
+
+const x =["3", "2", "4"];
+
+const y= ["2",
+"4",
+"7"];
+
+
+
+
+
+
+  
+
+
